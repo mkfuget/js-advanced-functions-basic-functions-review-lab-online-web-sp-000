94e41ec538357ffcca9c6087dfed78129545db11
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 05:33:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ const Calculator = {};
 Calculator.add = function(a, b){
   return a+ b;
 }
+Calculator.subtract = function(a, b){
+  return a- b;
+}
+Calculator.multiply = function(a, b){
+  return a*b;
+}
+Calculator.divide = function(a, b){
+  return a/b;
+}
